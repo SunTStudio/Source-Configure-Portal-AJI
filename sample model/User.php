82bc9c4eba01386bb95f82e368d2d4b88de899c7
SunTStudio <mahsunmuh0@gmail.com>
2024-11-12 15:35:56 +0700

    c

--- sample model/User.php
+++ sample model/User.php
@@ -40,29 +40,6 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function auditor() {
-        return $this->hasMany(laporan::class, 'auditor_id');
-    }
-
-    public function PIC() {
-        return $this->hasMany(laporan::class, 'PIC_id');
-    }
-
-    public function dept_PIC() {
-        return $this->hasMany(laporan::class, 'dept_pic_id');
-    }
-
-    public function dept_EHS() {
-        return $this->hasMany(laporan::class, 'dept_ehs_id');
-    }
-
-    public function area() {
-        return $this->hasMany(user_has_area::class, 'user_id');
-    }
-
-    public function team() {
-        return $this->hasMany(team_member::class, 'user_id');
-    }
 
     public function departement() {
         return $this->belongsTo(department::class, 'dept_id');
@@ -74,21 +51,5 @@ class User extends Authenticatable
 
     public function position() {
         return $this->belongsTo(position::class, 'position_id');
-    }
-
-    public function genba_member() {
-        return $this->hasMany(genba_detail::class, 'user_id');
-    }
-
-    public function PIC_auditor() {
-        return $this->hasMany(genba::class, 'pic_auditor_id');
-    }
-
-    public function auditor_log() {
-        return $this->hasMany(activity_log::class, 'auditor_id');
-    }
-
-    public function user_log() {
-        return $this->hasMany(activity_log::class, 'auditor_id');
     }
 }
